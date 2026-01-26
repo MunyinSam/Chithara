16baf81046e2a2f71e852e78a1799bb026b142fe
MunyinSam <kelvinsam233@gmail.com>
2026-01-26 18:51:05 +0700

    init base

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="">
+        <div className="text-2xl">
+          Type Shi
+        </div>
+      </main>
+    </div>
+  );
+}
