92f5ed92382d996eeafca7b9024c395f2202032c
MunyinSam <kelvinsam233@gmail.com>
2026-04-21 22:41:02 +0700

    change file name because  api/models.py (file) clashes with api/models/ (directory).

--- backend/api/models.py
+++
@@ -1,7 +0,0 @@
-from .models.User import User
-from .models.Song import Song
-from .models.GenerationHistory import GenerationHistory
-from .models.GenerationQuota import GenerationQuota
-
-# Just imports all db tables from /models
-__all__ = ['User', 'Song', 'GenerationHistory', 'GenerationQuota']
---
+++ backend/api/models/__init__.py
@@ -0,0 +1,6 @@
+from .User import User
+from .Song import Song
+from .GenerationHistory import GenerationHistory
+from .GenerationQuota import GenerationQuota
+
+__all__ = ['User', 'Song', 'GenerationHistory', 'GenerationQuota']
